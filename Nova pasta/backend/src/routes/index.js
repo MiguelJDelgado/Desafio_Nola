d9073697549f5d4d1be2d8113dad17cc24bfd220
MiguelJDelgado <migueljd193@gmail.com>
2025-10-31 21:07:18 -0300

    Correção do repositório git

--- Nova pasta/backend/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getAllStores } from "../controllers/StoreController.js";
-
-const router = express.Router();
-
-router.get("/stores", getAllStores);
-
-export default router;
