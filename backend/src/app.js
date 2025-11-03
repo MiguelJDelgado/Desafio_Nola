6fdee5d8326d37c8e7cc271f7a90bc12375802a5
MiguelJDelgado <migueljd193@gmail.com>
2025-11-03 04:32:30 -0300

    Instalação do cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import routes from "./routes/routes.js";
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
 app.use("/api", routes);
 
 export default app;
